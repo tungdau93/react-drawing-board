5186bae4962cd358d0bc58adaff1bb603d63cb99
tungdv <tungdv@vnext.com.vn>
2021-12-01 09:29:41 +0700

    change text for sketch.text

--- src/locales/ja.ts
+++ src/locales/ja.ts
@@ -2,7 +2,7 @@ export default {
   'umi.block.sketch.select': '選択',
   'umi.block.sketch.pencil': 'ペン',
   'umi.block.sketch.shape': '図形',
-  'umi.block.sketch.text': 'T テキスト',
+  'umi.block.sketch.text': 'テキスト',
   'umi.block.sketch.image': '画像',
   'umi.block.sketch.undo': '戻る',
   'umi.block.sketch.redo': '進む',
